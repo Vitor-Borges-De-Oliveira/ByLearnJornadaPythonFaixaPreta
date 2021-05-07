2fe480ad0a0503a1e429d97425092ef87f8bc589
Vitor Borges de Oliveira <83785633+Vitor-Borges-De-Oliveira@users.noreply.github.com>
2021-05-07 14:33:26 -0300

    Create redacao.py

---
+++ redacao.py
@@ -0,0 +1,37 @@
+# Frases pequenas
+
+nome = 'Felicia'
+sobrenome = 'Hawkins'
+idadefelicia = 23
+pai = 'Senhor'
+idadepai = 52
+mae = 'Senhora'
+idademae = 48
+parque = 'aquático'
+museu = 'de Nostradamus'
+cidade = 'Nostradamus'
+
+# Frases grandes
+
+rotina = 'pegar o ônibus 623 todos os dias para ir até'
+local = 'o trabalho'
+estacao = 'do terminal alvorada'
+preocupacao = 'preocupados por onde sua filha '
+caminho = 'passa todos os dias para'
+trabalho = 'ir ao trabalho'
+perigoso = 'Esta cidade é muito perigosa'
+bandido = 'existem bandidos por toda parte'
+medo = 'eles têm medo do que pode acontecer com seu bebê'
+anos = 'anos de idade'
+lugar = 'mais vivo e repleto de pessoas'
+rua = 'pela quebrada'
+
+# História completa
+
+print(f'{nome} {sobrenome} é uma garota comum que vive em {cidade}, ela tem {idadefelicia} e trabalha no museu {museu}.')
+print(f'Seus pais {pai} e {mae} {sobrenome} ficam extremamente {preocupacao} anda,')
+print(f'pórém {nome} é acostumada a {rotina} {local}.')
+print(f'Seus pais ficam {preocupacao} {caminho} {trabalho} pelo {estacao}, um lugar bem estranho.')
+print(f'{perigoso}, {bandido}, exclusivamente perigoso para alguém com {idadefelicia} {anos}.')
+print(f'Seus pais estão certos, {medo}, passar pelo parque {parque} seria melhor, é um lugar {lugar}.')
+print(f'Mas quem decide é ela, se ela prefere passar {rua} não há quem a proíba.')
